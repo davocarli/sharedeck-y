a13888d64c6bec55ad2d256498df47d4be824044
Giles Thompson <giles@papaiti.com>
2023-01-08 11:30:47 +1300

    chore(ignored apps): add Proton experimental and document app ids with comments

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,13 @@ export const SHAREDECK_REPORT_ENDPOINT =
 
 export const SHAREDECK_NEW_REPORT_URL =
 	"https://sharedeck.games/apps/${appid}/reports/new"
-export const ignoreSteam = [1887720, 1070560, 1391110, 228980]
+export const ignoreSteam = [
+  1887720, // Proton 7.0
+  1493710, // Proton Experimental
+  1070560, // Steam Linux Runtime
+  1391110, // Steam Linux Runtime - Soldier
+  228980, // Steamworks Common Redistributables
+]
 export const ignoreNonSteam = [
 	"EmulationStation-DE-x64_SteamDeck",
 	"Google Chrome",
