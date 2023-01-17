327c402081dd1171aa3b961fba0461ee50f8fd33
David Carli-Arnold <davocarli@gmail.com>
2023-01-17 08:49:38 -0300

    Add back button to settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -4,13 +4,15 @@ import {
 	PanelSectionRow,
 	ToggleField,
 } from "decky-frontend-lib"
+import React from "react"
 import { useContext, useState } from "react"
+import BackButton from "../components/backButton"
 import { PluginSettings, ShareDeckContext } from "../context"
 import { getSettings, sendSDHQToast, sendShareDeckToast } from "../requests"
 
 const SettingsPage = () => {
 	const [currentSettings, setCurrentSettings] = useState(() => getSettings())
-	const { serverApi } = useContext(ShareDeckContext)
+	const { serverApi, setShowSettings } = useContext(ShareDeckContext)
 
 	const updateSetting = (label: keyof PluginSettings, value: any) => {
 		currentSettings[label] = value
@@ -28,39 +30,45 @@ const SettingsPage = () => {
 	}
 
 	return (
-		<PanelSection title="Settings">
-			<PanelSectionRow>
-				<ToggleField
-					checked={currentSettings.showShareDeckToasts}
-					label="Show ShareDeck Notifications"
-					description="Send a notification when opening a game if ShareDeck reports are available."
-					onChange={(n) => updateSetting("showShareDeckToasts", n)}
-				/>
-			</PanelSectionRow>
-			<PanelSectionRow>
-				<ToggleField
-					checked={currentSettings.showSDHQToasts}
-					label="Show SteamDeckHQ Notifications"
-					description="Send a notification when opening a game if SteamDeckHQ has performance-reviewed the game."
-					onChange={(n) => updateSetting("showSDHQToasts", n)}
-				/>
-			</PanelSectionRow>
-			<PanelSectionRow>
-				<ToggleField
-					checked={!currentSettings.showAlways}
-					label="Only Show on First Open"
-					description="If enabled, you will only receive notifications the first time you open a game."
-					onChange={(n) => updateSetting("showAlways", !n)}
-				/>
-			</PanelSectionRow>
-			{serverApi ? (
+		<React.Fragment>
+			<BackButton onClick={() => setShowSettings(false)} />
+
+			<PanelSection title="Settings">
 				<PanelSectionRow>
-					<ButtonItem layout="below" onClick={() => sendToasts()}>
-						Test Notifications
-					</ButtonItem>
+					<ToggleField
+						checked={currentSettings.showShareDeckToasts}
+						label="Show ShareDeck Notifications"
+						description="Send a notification when opening a game if ShareDeck reports are available."
+						onChange={(n) =>
+							updateSetting("showShareDeckToasts", n)
+						}
+					/>
 				</PanelSectionRow>
-			) : null}
-		</PanelSection>
+				<PanelSectionRow>
+					<ToggleField
+						checked={currentSettings.showSDHQToasts}
+						label="Show SteamDeckHQ Notifications"
+						description="Send a notification when opening a game if SteamDeckHQ has performance-reviewed the game."
+						onChange={(n) => updateSetting("showSDHQToasts", n)}
+					/>
+				</PanelSectionRow>
+				<PanelSectionRow>
+					<ToggleField
+						checked={!currentSettings.showAlways}
+						label="Only Show on First Open"
+						description="If enabled, you will only receive notifications the first time you open a game."
+						onChange={(n) => updateSetting("showAlways", !n)}
+					/>
+				</PanelSectionRow>
+				{serverApi ? (
+					<PanelSectionRow>
+						<ButtonItem layout="below" onClick={() => sendToasts()}>
+							Test Notifications
+						</ButtonItem>
+					</PanelSectionRow>
+				) : null}
+			</PanelSection>
+		</React.Fragment>
 	)
 }
 
